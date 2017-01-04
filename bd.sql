f36b94e5552ac185d557953a7de07349ab001525
Gerard2702 <gerard.gt2009@gmail.com>
2017-01-04 15:32:16 -0600

    bd correcciones

--- bd.sql
+++ bd.sql
@@ -81,6 +81,7 @@ INSERT INTO `estado_equipo` (`id_estado_equipo`, `nombre`) VALUES
 CREATE TABLE IF NOT EXISTS `inventario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tipo_equipo` varchar(50) DEFAULT NULL,
+  `nivel` varchar(50) DEFAULT NULL,
   `ubicacion` varchar(50) DEFAULT NULL,
   `usuario` varchar(50) DEFAULT NULL,
   `centro_costo` int(50) DEFAULT NULL,
@@ -104,15 +105,15 @@ CREATE TABLE IF NOT EXISTS `inventario` (
   `fecha_vencimiento` varchar(50) DEFAULT NULL,
   `estado_equipo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
 
--- Dumping data for table invequipo.inventario: ~4 rows (approximately)
+-- Dumping data for table invequipo.inventario: ~42 rows (approximately)
 /*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
-INSERT INTO `inventario` (`id`, `tipo_equipo`, `ubicacion`, `usuario`, `centro_costo`, `numero_inventario`, `marca`, `modelo`, `serie`, `marca_modelo`, `velocidad`, `ram`, `hdd`, `cd_dvd`, `sistema_operativo`, `licencia`, `version_office`, `licencia_office`, `nombre_equipo`, `direccionip`, `nombre_dominio`, `fecha_adquisicion`, `fecha_vencimiento`, `estado_equipo`) VALUES
-	(1, 'Computadora de Escritorio', '7º Nivel - Jefatura de Consulta Externa', 'Licda. Sandra Martinez 2', 537205, '03-530-08419', 'HP', 'Elite Desk 705 G1 SFF', 'MXL4452PF8', 'AMD  A10 PRO-7800B R7', '3 GHz', '4 GB', '1 TB', 'DVD RW', 'Windows 8.1 Pro 64 bits SP1', '', '2013 Standard', 'Preinstalado', '', 'Dinámica (Asignada por DHCP)\n', 'isss.gob.sv\n', 'Diciembre 2014', 'Diciembre 2019', 'Óptimo'),
-	(2, 'Computadora de Escritorio', '7º Nivel - Oficina Jefatura de C. Ext. (Preparació', 'Licda. Sandra Martinez', 537208, '03-530-08434', 'HP', 'Elite Desk 705 G1 SFF', 'MXL446093V', 'AMD  A10 PRO-7800B R7', '3 GHz', '4 GB', '1 TB', 'DVD RW', 'Windows 8.1 Pro 64 bits SP1', '', '2013 Standard', 'Preinstalado', '', 'Dinámica (Asignada por DHCP)\n', 'isss.gob.sv\n', 'Diciembre 2014', 'Diciembre 2019', 'Óptimo'),
-	(3, 'Computadora de Escritorio\r\n', '1º Nivel - Informática (Clínica 2 Oftalmología - E', 'Ing. Sergio Danilo León (Dr. Douglas Mendoza)\r\n', 537307, '03-530-08539', 'HP', 'Elite Desk 705 G1 SFF', 'MXL4460W0Q', 'AMD \nA10 PRO-7800B R7', '3.50 GHz', '4 GB', '1 TB', 'DVD RW', '"Windows 8.1 Pro\n64 bits\nSP1"', NULL, 'No tiene Office instalado', 'No tiene Office instalado', NULL, 'Dinámica (Asignada por DHCP)', 'isss.gob.sv', 'Diciembre 2014', 'Diciembre 2019', 'Óptimo'),
-	(6, 'Computadora de Escritorio', '3º Nivel - Farmacia Jefatura', 'Lic. Elizabeth Varela', 53703, '03-530-06265', 'DELL\n', 'OPTIPLEX 780', 'D30FNN1', 'Intel Core 2 Duo', '2.93 GHz', '4 GB', '300 GB', 'DVD\n', 'Windows 7 Professional', 'C74YD-2VXG6-3XGWP-JJW7K-FVC2M', 'Office 2010', 'Preinstalado', 'CE-FARMA-6265', 'Dinámica (Asignada por DHCP)\n', 'isss.gob.sv\n', '06/Diciembre/2010', '06/Diciembre/2013', 'Óptimo');
+INSERT INTO `inventario` (`id`, `tipo_equipo`, `nivel`, `ubicacion`, `usuario`, `centro_costo`, `numero_inventario`, `marca`, `modelo`, `serie`, `marca_modelo`, `velocidad`, `ram`, `hdd`, `cd_dvd`, `sistema_operativo`, `licencia`, `version_office`, `licencia_office`, `nombre_equipo`, `direccionip`, `nombre_dominio`, `fecha_adquisicion`, `fecha_vencimiento`, `estado_equipo`) VALUES
+	(1, 'Computadora de Escritorio', NULL, '7º Nivel - Jefatura de Consulta Externa', 'Licda. Sandra Martinez 2', 537205, '03-530-08419', 'HP', 'Elite Desk 705 G1 SFF', 'MXL4452PF8', 'AMD  A10 PRO-7800B R7', '3 GHz', '4 GB', '1 TB', 'DVD RW', 'Windows 8.1 Pro 64 bits SP1', '', '2013 Standard', 'Preinstalado', '', 'Dinámica (Asignada por DHCP)\n', 'isss.gob.sv\n', 'Diciembre 2014', 'Diciembre 2019', 'Óptimo'),
+	(2, 'Computadora de Escritorio', NULL, '7º Nivel - Oficina Jefatura de C. Ext. (Preparació', 'Licda. Sandra Martinez', 537208, '03-530-08434', 'HP', 'Elite Desk 705 G1 SFF', 'MXL446093V', 'AMD  A10 PRO-7800B R7', '3 GHz', '4 GB', '1 TB', 'DVD RW', 'Windows 8.1 Pro 64 bits SP1', '', '2013 Standard', 'Preinstalado', '', 'Dinámica (Asignada por DHCP)\n', 'isss.gob.sv\n', 'Diciembre 2014', 'Diciembre 2019', 'Óptimo'),
+	(3, 'Computadora de Escritorio\r\n', NULL, '1º Nivel - Informática (Clínica 2 Oftalmología - E', 'Ing. Sergio Danilo León (Dr. Douglas Mendoza)\r\n', 537307, '03-530-08539', 'HP', 'Elite Desk 705 G1 SFF', 'MXL4460W0Q', 'AMD \nA10 PRO-7800B R7', '3.50 GHz', '4 GB', '1 TB', 'DVD RW', '"Windows 8.1 Pro\n64 bits\nSP1"', NULL, 'No tiene Office instalado', 'No tiene Office instalado', NULL, 'Dinámica (Asignada por DHCP)', 'isss.gob.sv', 'Diciembre 2014', 'Diciembre 2019', 'Óptimo'),
+	(6, 'Computadora de Escritorio', NULL, '3º Nivel - Farmacia Jefatura', 'Lic. Elizabeth Varela', 53703, '03-530-06265', 'DELL\n', 'OPTIPLEX 780', 'D30FNN1', 'Intel Core 2 Duo', '2.93 GHz', '4 GB', '300 GB', 'DVD\n', 'Windows 7 Professional', 'C74YD-2VXG6-3XGWP-JJW7K-FVC2M', 'Office 2010', 'Preinstalado', 'CE-FARMA-6265', 'Dinámica (Asignada por DHCP)\n', 'isss.gob.sv\n', '06/Diciembre/2010', '06/Diciembre/2013', 'Óptimo');
 /*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
 
 -- Dumping structure for table invequipo.marca
@@ -133,6 +134,25 @@ INSERT INTO `marca` (`id_marca`, `nombre`) VALUES
 	(6, 'TOSHIBA');
 /*!40000 ALTER TABLE `marca` ENABLE KEYS */;
 
+-- Dumping structure for table invequipo.nivel
+CREATE TABLE IF NOT EXISTS `nivel` (
+  `id_nivel` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_nivel`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table invequipo.nivel: ~0 rows (approximately)
+/*!40000 ALTER TABLE `nivel` DISABLE KEYS */;
+INSERT INTO `nivel` (`id_nivel`, `nombre`) VALUES
+	(1, '7 Nivel - Consulta Externa'),
+	(2, '6 Nivel - Administrativo'),
+	(3, '6 Nivel - Consulta Externa'),
+	(4, '5 Nivel - Consulta Externa'),
+	(5, '5 Nivel - Rayos X'),
+	(8, '4 Nivel - Consulta Externa'),
+	(9, '4 Nivel - Procedimientos');
+/*!40000 ALTER TABLE `nivel` ENABLE KEYS */;
+
 -- Dumping structure for table invequipo.observaciones
 CREATE TABLE IF NOT EXISTS `observaciones` (
   `id_observacion` int(11) NOT NULL AUTO_INCREMENT,
@@ -141,9 +161,9 @@ CREATE TABLE IF NOT EXISTS `observaciones` (
   PRIMARY KEY (`id_observacion`),
   KEY `FK_observaciones_inventario` (`id_inventario`),
   CONSTRAINT `FK_observaciones_inventario` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
 
--- Dumping data for table invequipo.observaciones: ~9 rows (approximately)
+-- Dumping data for table invequipo.observaciones: ~38 rows (approximately)
 /*!40000 ALTER TABLE `observaciones` DISABLE KEYS */;
 INSERT INTO `observaciones` (`id_observacion`, `id_inventario`, `nombre`) VALUES
 	(1, 1, 'Serie teclado: BDMEP0AHH6W0CG'),
@@ -162,15 +182,17 @@ CREATE TABLE IF NOT EXISTS `otros_software` (
   PRIMARY KEY (`id_otro`),
   KEY `FK_otros_software_inventario` (`id_inventario`),
   CONSTRAINT `FK_otros_software_inventario` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
 
--- Dumping data for table invequipo.otros_software: ~5 rows (approximately)
+-- Dumping data for table invequipo.otros_software: ~42 rows (approximately)
 /*!40000 ALTER TABLE `otros_software` DISABLE KEYS */;
 INSERT INTO `otros_software` (`id_otro`, `id_inventario`, `nombre`) VALUES
 	(1, 1, 'Software Preinstalado'),
 	(3, 2, 'Software Preinstalado'),
 	(5, 3, 'Software Preinstalado'),
-	(8, 6, 'Software Preinstalado');
+	(8, 6, 'Software Preinstalado'),
+	(9, 6, 'Software Preinstalado'),
+	(10, 6, 'Software Preinstalado');
 /*!40000 ALTER TABLE `otros_software` ENABLE KEYS */;
 
 -- Dumping structure for table invequipo.sistemas_institucionales
@@ -181,17 +203,21 @@ CREATE TABLE IF NOT EXISTS `sistemas_institucionales` (
   PRIMARY KEY (`id_sistema`),
   KEY `FK__inventario` (`id_inventario`),
   CONSTRAINT `FK__inventario` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
 
--- Dumping data for table invequipo.sistemas_institucionales: ~7 rows (approximately)
+-- Dumping data for table invequipo.sistemas_institucionales: ~44 rows (approximately)
 /*!40000 ALTER TABLE `sistemas_institucionales` DISABLE KEYS */;
 INSERT INTO `sistemas_institucionales` (`id_sistema`, `id_inventario`, `nombre`) VALUES
 	(1, 1, 'Sistema de Agenda Médica '),
-	(2, 1, 'Web Consulta de Afiliados '),
-	(3, 1, 'Sistema de Patología"'),
+	(2, 1, '\nWeb Consulta de Afiliados '),
+	(3, 1, '\nSistema de Patología"'),
 	(5, 2, 'Modulo de Consulta Externa'),
 	(7, 3, 'Modulo de Consulta Externa'),
-	(9, 6, 'SAP (SAFISSS)');
+	(9, 6, 'SAP (SAFISSS)'),
+	(10, 6, 'Sistema de Agenda Médica '),
+	(11, 6, '\nWeb Consulta de Afiliados'),
+	(12, 6, 'Sistema de Agenda Médica '),
+	(13, 6, '\nWeb Consulta de Afiliados');
 /*!40000 ALTER TABLE `sistemas_institucionales` ENABLE KEYS */;
 
 -- Dumping structure for table invequipo.tipo_equipo
